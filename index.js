a5cf93b8ea62c3e322e591842e07ac86d430f876
Rohit <rohit712wd@gmail.com>
2022-05-16 18:56:14 +0530

    add-item function add complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const readline = require("readline");
-const PATH_TXT_TASK = ".text-files/task.txt";
-const PATH_TXT_COMPLETED = ".text-files/completed.txt";
+const PATH_TXT_TASK = "./text-files/task.txt";
+const PATH_TXT_COMPLETED = "./text-files/completed.txt";
 const spawn = require("child_process").spawn;
 const sort = spawn("sort", ["-k1,1", PATH_TXT_TASK]);
 const sort2 = spawn("sort", ["-k1,1", PATH_TXT_COMPLETED]);
@@ -10,6 +10,31 @@ let n = 0;
 let m = 0;
 let pending = [];
 let completed = [];
+let command = process.argv[2];
+
+switch (command) {
+  case "help":
+    usage();
+    break;
+  case "ls":
+    listAllPending();
+    break;
+  case "add":
+    add();
+    break;
+  case "del":
+    del();
+    break;
+  case "done":
+    done();
+    break;
+  case "report":
+    report();
+    break;
+  default:
+    usage();
+    break;
+}
 
 function usage() {
   console.log(`
@@ -32,4 +57,27 @@ Usage :-
 >./todolist report
 # Statistics.
 `);
+}
+
+function add() {
+  let priority = process.argv[3];
+  let task = process.argv[4];
+
+  if (task == undefined) {
+    console.error("Error: Missing tasks string. Nothing added!");
+    return;
+  } else if (priority == undefined || isNaN(priority)) {
+    console.error("priority must be number");
+    return;
+  } else if (priority < 0) {
+    console.error("priority cant be less than 0");
+    return;
+  }
+
+  var logger = fs.createWriteStream(PATH_TXT_TASK, {
+    flags: "a",
+  });
+
+  logger.write("[" + priority + "]" + " " + task + "\n");
+  console.log('Added task: "' + task + '" with priority [' + priority + "]");
 }
